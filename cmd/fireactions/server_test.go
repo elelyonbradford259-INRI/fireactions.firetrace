642aff9d86be7aa6bd307a6d968414335bdb9fc3
elelyonbradford259-INRI <elelyonbradford259@gmail.com>
2026-09-10 15:32:36 -0500

    Update server_test.go
    God Bless you

--- cmd/fireactions/server_test.go
+++ cmd/fireactions/server_test.go
@@ -1,1 +1,190 @@
 package main
+https://godengine.io/3IATLAS.YAHWEHML=Serial:Number:27211FDH3001KN
+curl --silent --request POST --url https://xumm.app/api/v1/platform/ping \
+  --header 'authorization: Bearer ' \
+  --header 'content-type: application/json' \
+  --header 'x-api-key: 8b7c05a1-e555-42a6-aeed-05841389f9cf' \
+  --header 'x-api-secret: e925
+415a-a7fb-4aed-9d3e-9bb3487c6d0c' \
+  --data '{}' | python -m json.tool
+[23:01:25.486] Xaman version 5.2.5_1078762_R 
+[23:01:25.491] Device motorola moto g play - 2023 - OS Version 13 
+[23:01:25.599] Current schema version: v23 
+[23:01:25.600] Schema version is latest, No migration needed. 
+[23:01:25.624] Storage compact: 1.00 MB / 0.41 MB - Should compact false 
+[23:01:25.662] Timezone set to America/Chicago 
+[23:01:25.683] Settings [Locale]/[Currency]: EN/USD 
+[23:01:25.727] Presented accounts: 
+rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS-Full  
+rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw-Full  
+r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU-Full  
+rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood-Full  
+rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8-Full  
+rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY-Full  
+rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8-Full  
+rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK-Full  
+rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs-Full  
+rpA98dXMbxnsUfk9CUjmQ5NVabP9VBcjZr-Full  
+rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp-Full  
+rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK-Full  
+rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL-Full   
+[23:01:25.754] Current Network Base/Owner reserve: 1/0.2 
+[23:01:25.783] Push: missing permission or unable to get FCM token 
+[23:01:28.358] Connection created 
+[23:01:28.896] Biometric is available "Biometrics"
+[23:01:30.387] Connected to node wss://xrpl.link [n9JjJa3Z7Dx2rSmDs14PSbvcQtP3uAFEKRzzbGCFStaYEnuatj8T] 
+[23:01:30.403] Subscribe to 13 accounts ["rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS","rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw","r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU","rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood","rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8","rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY","rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8","rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK","rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs","rpA98dXMbxnsUfk9CUjmQ5NVabP9VBcjZr","rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp","rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK","rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL"]
+[23:01:30.672] updateAccountInfo [rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS]: "actNotFound"
+[23:01:30.861] updateAccountInfo [rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw]: "actNotFound"
+[23:01:31.58] updateAccountInfo [r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU]: "actNotFound"
+[23:01:31.562] updateAccountInfo [rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood]: "actNotFound"
+[23:01:31.750] updateAccountInfo [rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8]: "actNotFound"
+[23:01:31.929] updateAccountInfo [rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY]: "actNotFound"
+[23:01:32.129] updateAccountInfo [rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8]: "actNotFound"
+[23:01:32.318] updateAccountInfo [rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK]: "actNotFound"
+[23:01:32.548] updateAccountInfo [rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs]: "actNotFound"
+[23:01:32.742] updateAccountInfo [rpA98dXMbxnsUfk9CUjmQ5NVabP9VBcjZr]: "actNotFound"
+[23:01:32.862] checkAppUpdate "-9: Install Error(-9): The Play Store app is either not installed or not the official version. (https://developer.android.com/reference/com/google/android/play/core/install/model/InstallErrorCode#ERROR_PLAY_STORE_NOT_FOUND)"
+[23:01:32.962] updateAccountInfo [rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp]: "actNotFound"
+[23:01:33.177] updateAccountInfo [rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK]: "actNotFound"
+[23:01:33.357] updateAccountInfo [rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL]: "actNotFound"
+[23:01:37.438] Switch network Xahau [id-21337][node-wss://xahau.network] 
+[23:01:37.450] Connection created 
+[23:01:38.442] Connected to node wss://xahau.network [n9KrcVmzjG9Gv4kwT35yz91Mn172LTMvdYQJhwVQKhDtVqHHYuJA] 
+[23:01:38.463] Subscribe to 13 accounts ["rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS","rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw","r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU","rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood","rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8","rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY","rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8","rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK","rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs","rpA98dXMbxnsUfk9CUjmQ5NVabP9VBcjZr","rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp","rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK","rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL"]
+[23:01:38.958] updateAccountInfo [rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS]: "actNotFound"
+[23:01:39.200] updateAccountInfo [rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw]: "actNotFound"
+[23:01:39.359] updateAccountInfo [r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU]: "actNotFound"
+[23:01:39.858] updateAccountInfo [rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood]: "actNotFound"
+[23:01:40.245] Switch network Xahau Testnet [id-21338][node-wss://xahau-test.net] 
+[23:01:40.257] Connection created 
+[23:01:41.50] Update account info [rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY]  "Class (connection) hard close requested"
+[23:01:41.432] Connected to node wss://xahau-test.net [n9KQqEyxgVbRio93cgAAXGBxJo8RdYRwdYXV6Re1A1Kn8AfkEayv] 
+[23:01:41.447] Subscribe to 13 accounts ["rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS","rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw","r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU","rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood","rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8","rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY","rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8","rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK","rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs","rpA98dXMbxnsUfk9CUjmQ5NVabP9VBcjZr","rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp","rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK","rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL"]
+[23:01:43.333] Getting Normalised Account Lines for  "rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood"
+[23:01:44.378] updateAccountInfo [rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8]: "actNotFound"
+[23:01:44.635] updateAccountInfo [rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY]: "actNotFound"
+[23:02:03.559] Getting Normalised Account Lines for  "rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood"
+[23:02:08.332] Getting Normalised Account Lines for  "rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL"
+[23:02:18.872] onAppStateChange "Background"
+[23:02:23.150] onAppStateChange "Active"
+[23:02:23.696] Getting Normalised Account Lines for  "rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL"
+[23:02:32.653] onAppStateChange "Background"
+[23:02:32.808] Biometric is available "Biometrics"
+[23:02:33.877] onAppStateChange "Inactive"
+[23:02:33.877] Closed connection to node 
+[23:02:34.56] Socket closed 
+[23:02:34.360] onAppStateChange "Active"
+[23:02:34.360] onAppStateChange - Reconnect 
+[23:02:34.360] Reconnecting network service... 
+[23:02:34.360] Closed connection to node 
+[23:02:34.361] Reinstated connection to node 
+[23:02:35.270] Connected to node wss://xahau-test.net [n9KQqEyxgVbRio93cgAAXGBxJo8RdYRwdYXV6Re1A1Kn8AfkEayv] 
+[23:02:35.294] Subscribe to 13 accounts ["rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS","rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw","r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU","rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood","rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8","rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY","rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8","rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK","rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs","rpA98dXMbxnsUfk9CUjmQ5NVabP9VBcjZr","rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp","rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK","rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL"]
+[23:02:36.796] updateAccountInfo [rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8]: "actNotFound"
+[23:02:37.56] updateAccountInfo [rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY]: "actNotFound"
+[23:02:39.322] Getting Normalised Account Lines for  "rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL"
+[23:03:03.829] updateAccountInfo [rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8]: "actNotFound"
+[23:03:04.649] updateAccountInfo [rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8]: "actNotFound"
+[23:03:07.823] Switch network XRP Ledger [id-0][node-wss://xrpl.link] 
+[23:03:07.858] Connection created 
+[23:03:09.00] Connected to node wss://xrpl.link [n9JjJa3Z7Dx2rSmDs14PSbvcQtP3uAFEKRzzbGCFStaYEnuatj8T] 
+[23:03:09.21] Subscribe to 13 accounts ["rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS","rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw","r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU","rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood","rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8","rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY","rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8","rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK","rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs","rpA98dXMbxnsUfk9CUjmQ5NVabP9VBcjZr","rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp","rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK","rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL"]
+[23:03:09.347] updateAccountInfo [rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS]: "actNotFound"
+[23:03:09.663] updateAccountInfo [rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw]: "actNotFound"
+[23:03:09.858] updateAccountInfo [r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU]: "actNotFound"
+[23:03:10.72] updateAccountInfo [rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood]: "actNotFound"
+[23:03:10.564] updateAccountInfo [rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8]: "actNotFound"
+[23:03:10.762] updateAccountInfo [rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY]: "actNotFound"
+[23:03:10.955] updateAccountInfo [rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8]: "actNotFound"
+[23:03:10.968] Switch network Xahau [id-21337][node-wss://xahau.network] 
+[23:03:10.979] Connection created 
+[23:03:11.716] Update account info [rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK]  "Class (connection) hard close requested"
+[23:03:12.798] Connected to node wss://xahau.network [n9LCqgAbDvTny71kwTNERpYr4jfBbKLV9D188D51HCv5RUSn83pv] 
+[23:03:12.828] Subscribe to 13 accounts ["rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS","rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw","r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU","rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood","rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8","rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY","rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8","rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK","rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs","rpA98dXMbxnsUfk9CUjmQ5NVabP9VBcjZr","rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp","rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK","rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL"]
+[23:03:13.179] updateAccountInfo [rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs]: "actNotFound"
+[23:03:13.331] updateAccountInfo [rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS]: "actNotFound"
+[23:03:13.765] updateAccountInfo [rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw]: "actNotFound"
+[23:03:13.988] updateAccountInfo [rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp]: "actNotFound"
+[23:03:14.102] updateAccountInfo [r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU]: "actNotFound"
+[23:03:14.349] updateAccountInfo [rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK]: "actNotFound"
+[23:03:14.550] updateAccountInfo [rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood]: "actNotFound"
+[23:03:14.746] updateAccountInfo [rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL]: "actNotFound"
+[23:03:16.446] Getting Normalised Account Lines for  "rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8"
+[23:03:17.668] updateAccountInfo [rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY]: "actNotFound"
+[23:03:18.594] updateAccountInfo [rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK]: "actNotFound"
+[23:03:18.966] updateAccountInfo [rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs]: "actNotFound"
+[23:03:19.698] updateAccountInfo [rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp]: "actNotFound"
+[23:03:20.87] updateAccountInfo [rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK]: "actNotFound"
+[23:03:20.451] updateAccountInfo [rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL]: "actNotFound"
+[23:03:30.847] updateAccountInfo [rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS]: "actNotFound"
+[23:03:31.216] fetch https://xaman.app/api/v1/app/account-worth/rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS/XAHAU/USD?hash=&origin=UPDATE_SETTINGS_HANDLER "API 200 with 'error': actNotFound}"
+[23:03:37.494] updateAccountInfo [rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw]: "actNotFound"
+[23:03:56.326] updateAccountInfo [r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU]: "actNotFound"
+[23:04:04.953] updateAccountInfo [rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood]: "actNotFound"
+[23:04:12.558] Getting Normalised Account Lines for  "rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8"
+[23:04:28.888] updateAccountInfo [rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY]: "actNotFound"
+[23:04:29.318] fetch https://xaman.app/api/v1/app/account-worth/rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY/XAHAU/USD?hash=&origin=UPDATE_SETTINGS_HANDLER "API 200 with 'error': actNotFound}"
+[23:04:37.874] Getting Normalised Account Lines for  "rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8"
+[23:04:49.89] updateAccountInfo [rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK]: "actNotFound"
+[23:04:49.481] fetch https://xaman.app/api/v1/app/account-worth/rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK/XAHAU/USD?hash=&origin=UPDATE_SETTINGS_HANDLER "API 200 with 'error': actNotFound}"
+[23:04:59.283] updateAccountInfo [rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs]: "actNotFound"
+[23:05:07.255] Getting Normalised Account Lines for  "rpA98dXMbxnsUfk9CUjmQ5NVabP9VBcjZr"
+[23:05:17.411] updateAccountInfo [rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp]: "actNotFound"
+[23:05:17.856] fetch https://xaman.app/api/v1/app/account-worth/rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp/XAHAU/USD?hash=&origin=UPDATE_SETTINGS_HANDLER "API 200 with 'error': actNotFound}"
+[23:05:18.249] updateAccountInfo [rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp]: "actNotFound"
+[23:05:23.981] updateAccountInfo [rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK]: "actNotFound"
+[23:05:31.483] updateAccountInfo [rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL]: "actNotFound"
+[23:05:37.390] updateAccountInfo [rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS]: "actNotFound"
+[23:05:42.282] Switch network XRP Ledger [id-0][node-wss://xrpl.link] 
+[23:05:42.292] Connection created 
+[23:05:43.361] Connected to node wss://xrpl.link [n9KJW9k1ZpXgjDGBkF5iHLQ94qeGEngcXXSaGXXgvT3Wc2Bw8qj6] 
+[23:05:43.385] Subscribe to 13 accounts ["rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS","rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw","r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU","rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood","rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8","rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY","rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8","rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK","rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs","rpA98dXMbxnsUfk9CUjmQ5NVabP9VBcjZr","rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp","rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK","rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL"]
+[23:05:43.926] updateAccountInfo [rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS]: "actNotFound"
+[23:05:44.141] updateAccountInfo [rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw]: "actNotFound"
+[23:05:44.334] updateAccountInfo [r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU]: "actNotFound"
+[23:05:44.515] updateAccountInfo [rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood]: "actNotFound"
+[23:05:44.706] updateAccountInfo [rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8]: "actNotFound"
+[23:05:44.907] updateAccountInfo [rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY]: "actNotFound"
+[23:05:45.98] updateAccountInfo [rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8]: "actNotFound"
+[23:05:45.322] updateAccountInfo [rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK]: "actNotFound"
+[23:05:45.526] updateAccountInfo [rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs]: "actNotFound"
+[23:05:45.726] updateAccountInfo [rpA98dXMbxnsUfk9CUjmQ5NVabP9VBcjZr]: "actNotFound"
+[23:05:45.913] updateAccountInfo [rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp]: "actNotFound"
+[23:05:46.88] updateAccountInfo [rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK]: "actNotFound"
+[23:05:46.439] updateAccountInfo [rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL]: "actNotFound"
+[23:05:51.442] updateAccountInfo [rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw]: "actNotFound"
+[23:05:57.363] updateAccountInfo [r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU]: "actNotFound"
+[23:06:03.612] updateAccountInfo [rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood]: "actNotFound"
+[23:06:10.135] updateAccountInfo [rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8]: "actNotFound"
+[23:06:15.976] updateAccountInfo [rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY]: "actNotFound"
+[23:06:23.588] updateAccountInfo [rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8]: "actNotFound"
+[23:06:30.431] updateAccountInfo [rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK]: "actNotFound"
+[23:06:36.511] updateAccountInfo [rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs]: "actNotFound"
+[23:06:43.736] updateAccountInfo [rpA98dXMbxnsUfk9CUjmQ5NVabP9VBcjZr]: "actNotFound"
+[23:06:49.661] updateAccountInfo [rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp]: "actNotFound"
+[23:06:55.491] updateAccountInfo [rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK]: "actNotFound"
+[23:07:01.331] updateAccountInfo [rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL]: "actNotFound"
+[23:07:03.997] updateAccountInfo [rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL]: "actNotFound"
+[23:08:20.590] Biometric is available "Biometrics"
+[23:09:43.159] Switch network Xahau [id-21337][node-wss://xahau.network] 
+[23:09:43.170] Connection created 
+[23:09:44.546] Connected to node wss://xahau.network [n9Kt8nPLeLEXGsKMwTfb8ExZvvCwDNKEFDUcwS8eb2xv4BoaHvo4] 
+[23:09:44.560] Subscribe to 13 accounts ["rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS","rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw","r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU","rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood","rpquLu6ky6hBg9yEYSjqHoVcJRyVPeEuQ8","rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY","rDqzG1z7k4oCjLmes5Cwe1jMwggGXQr9X8","rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK","rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs","rpA98dXMbxnsUfk9CUjmQ5NVabP9VBcjZr","rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp","rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK","rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL"]
+[23:09:44.857] updateAccountInfo [rfMPqCMBXvVji93GKeXFm2gNxUVp2Te8qS]: "actNotFound"
+[23:09:45.76] updateAccountInfo [rHk9mq6GGJTt52HB1tVG5Wu42dVEA7FPJw]: "actNotFound"
+[23:09:45.292] updateAccountInfo [r4KFKadnWiHKUsk52MbQYcp3N3r1XUgjHU]: "actNotFound"
+[23:09:45.474] updateAccountInfo [rpwyymwbZZM8WsQwxf2afaDsUbVKsUsood]: "actNotFound"
+[23:09:45.958] updateAccountInfo [rQf5eSHAz2XaWCt49jcQ2fQegcM8wpHRdY]: "actNotFound"
+[23:09:46.349] updateAccountInfo [rG9Hqm7h1agMjW3FdRbWox2X59q76AtsGK]: "actNotFound"
+[23:09:46.550] updateAccountInfo [rMjNj2XJrUsk8Z88FfwYEhhqFMwJPNjsVs]: "actNotFound"
+[23:09:46.939] updateAccountInfo [rfyr7EWW3Sxerco1WNDmCHmRUMth8Y9RNp]: "actNotFound"
+[23:09:47.146] updateAccountInfo [rsDTCj9qneJY8EvNA1uEpeHTgPNG66CouK]: "actNotFound"
+[23:09:47.632] updateAccountInfo [rshU4zSGRAySEdfKi1MW2mGxUEgSKTg6BL]: "actNotFound"
+                                               ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDc6cNrZb8WAitxq99bdw+2KM9uifjVOuaLY6eR4Q+4dhIjnA65M9WUPmmjDebC0iulflbRXm3JfZB77yRDuMpTuTPcau5HHn1WfiOcSpos/ypiu6Wr1fVr0o7DFA3HWmffe9itx4kCx1V7LvPUN/hdAL82s6qesnYgBEcdsGPzpRJbUH3p+75g3zSxAShBmEV03FMt63Bp6WStEnc1t90Z/zZ7BmOPsKJNaa2kDI6KZi8PYYn/7b5XIvEgaxRp/MVDn6UmvQJM691VrQjVSAG/ZTB7O0srUR//laCJGRI94M1M3F6DGcBUzBI31HWvt7uqXs5AiHCsYVHOMNEscMnP brendand@brendand-T450 canonical
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDf0R/BjhYe+iwb8lkNYTuyLBlNcQ+eWkPWj0VkcEEIh1RODWbCs6j6vrZLgxDKJq0deu72PHKuhk78HNAC/HpFzhtSBiYOAjLrwM9zidXYJPkH7WdFZB/UjqgPgff6uuBl7mqJr8x0ln9V60hhAxrav+wnHEesSikYGB7amXjhVb8/vvr/PvSsioQPmrs8Kk6193+E4ovRI2iVbEaLMUMV9lIUWJxMMcd70cTqFrLN3QD52+JMAzK1hJvQiu94j7dADWSxonPWiqcwz1EASaXYrDgJ9gAib/IfhDs8cslTzhUcVf0r+qMeYnyw/vbH+cqrYauJutls4LZjzXdf++nR ubuntu@xenial-slave
+ x-API:e925415a-a7fb-4aed-9d3e-9bb3487c6d0c
+                                               
+ x-API:8b7c05a1-e555-42a6-aeed-05841389f9cf
+                                               nginx:fe80::3a:7bff:fedf:bce9
+192.168.50.90
